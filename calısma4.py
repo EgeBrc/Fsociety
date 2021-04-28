6142ecfa72717cd8b9191efe151dff4022e268bb
EgeBrc <83343520+EgeBrc@users.noreply.github.com>
2021-04-28 15:02:11 +0300

    Add files via upload
    Fsociety Etkinlik 4

---
+++ calısma4.py
@@ -0,0 +1,14 @@
+print("Kullanıcı adını giriniz . . .")
+ka=input()
+if ka=="Ege":
+    print("Başarılı")
+    print("Şifrenizi Giriniz") 
+else: 
+    print("Başarısız")
+
+s=input()
+if s=="sawq0348":
+    print("Giriş Başarılı")
+else : 
+    print("Giriş Başarısız")
+
